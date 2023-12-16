087fe5f98894190d9f3dbe754fec708529074d3f
a.k.alehin <a.k.alehin@gmail.com>
2023-12-16 16:57:46 +0300

    add, delete

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,7 +1,8 @@
 from django.http import Http404, HttpResponseNotFound
 from django.shortcuts import render, redirect
-from MainApp.models import Snippet
 from django.core.exceptions import ObjectDoesNotExist
+from MainApp.models import Snippet
+from MainApp.forms import SnippetForm
 
 
 def index_page(request):
@@ -9,14 +10,10 @@ def index_page(request):
     return render(request, 'pages/index.html', context)
 
 
-def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
-    return render(request, 'pages/add_snippet.html', context)
-
-
 def snippets_page(request):
     context = {"snippets": Snippet.objects.all(), 'pagename': 'Просмотр сниппетов'}
     return render(request, 'pages/view_snippets.html', context)
+
 
 def snippet_detail_page(request, id):
     try:
@@ -24,4 +21,31 @@ def snippet_detail_page(request, id):
     except ObjectDoesNotExist:
         return HttpResponseNotFound(f'Сниппет с id={id} не найден')
     context = {"snippet": snippet}
-    return render(request, "pages/detail_snippet.html", context)  
+    return render(request, "pages/detail_snippet.html", context)
+
+
+def add_snippet_page(request):
+    if request.method == "GET":
+        form = SnippetForm()
+        context = {
+                'form': form,
+                'pagename': 'Добавление нового сниппета'}
+        return render(request, 'pages/add_snippet.html', context)
+    if request.method == "POST":
+       form = SnippetForm(request.POST)
+       if form.is_valid():
+           form.save()
+           return redirect("snippets_list")
+    return render(request,'pages/add_snippet.html',{'form': form, 'pagename': 'Надо исправить'})
+
+
+def snippet_delete(request, id):
+    try:
+        snippet = Snippet.objects.get(id=id)
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound(f'Сниппет с id={id} не найден')
+    if request.method =="POST":
+        snippet.delete()
+        return redirect('snippets_list')
+    context = {"snippet": snippet}
+    return render(request, "pages/detail_snippet.html", context)
--- Snippets/urls.py
+++ Snippets/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('snippets/add', views.add_snippet_page, name='snippets_add'),
     path('snippets/list', views.snippets_page, name='snippets_list'),
     path('snippet/<int:id>', views.snippet_detail_page, name='snippet_detail'),
+    #path('snippet/create', views.snippet_create, name='snippet_create'),
+    path('snippet/delete/<int:id>', views.snippet_delete, name='snippet_delete'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
