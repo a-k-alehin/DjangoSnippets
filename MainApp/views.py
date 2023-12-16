30f6a97647d19e9f0e62014269b8e2b32d0f3d57
a.k.alehin <a.k.alehin@gmail.com>
2023-12-16 18:00:54 +0300

    edit

--- MainApp/views.py
+++ MainApp/views.py
@@ -16,12 +16,36 @@ def snippets_page(request):
 
 
 def snippet_detail_page(request, id):
+    if request.method == "GET":
+        try:
+            snippet = Snippet.objects.get(id=id)
+        except ObjectDoesNotExist:
+            return HttpResponseNotFound(f'Сниппет с id={id} не найден')
+        context = {"snippet": snippet, "can_edit": False}
+        return render(request, "pages/detail_snippet.html", context)
+    return HttpResponseNotAllowed()
+
+
+def snippet_edit_page(request, id):
     try:
         snippet = Snippet.objects.get(id=id)
     except ObjectDoesNotExist:
         return HttpResponseNotFound(f'Сниппет с id={id} не найден')
-    context = {"snippet": snippet}
-    return render(request, "pages/detail_snippet.html", context)
+    if request.method == "GET":
+        context = {"snippet": snippet, "can_edit": True}
+        return render(request, "pages/detail_snippet.html", context)
+    if request.method == "POST":
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            snippet.update(
+                name=request.POST.get('name',snippet.name),
+                lang=request.POST.get('lang',snippet.lang),
+                code=request.POST.get('code',snippet.code)
+                )
+            snippet.save()
+            return redirect("snippets_list")
+        return render(request,'pages/detail_snippet.html',{'form': form, 'pagename': 'Надо исправить'})
+    return HttpResponseNotAllowed("")
 
 
 def add_snippet_page(request):
@@ -32,11 +56,12 @@ def add_snippet_page(request):
                 'pagename': 'Добавление нового сниппета'}
         return render(request, 'pages/add_snippet.html', context)
     if request.method == "POST":
-       form = SnippetForm(request.POST)
-       if form.is_valid():
-           form.save()
-           return redirect("snippets_list")
-    return render(request,'pages/add_snippet.html',{'form': form, 'pagename': 'Надо исправить'})
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("snippets_list")
+        return render(request,'pages/add_snippet.html',{'form': form, 'pagename': 'Надо исправить'})
+    return HttpResponseNotAllowed('')
 
 
 def snippet_delete(request):
--- Snippets/urls.py
+++ Snippets/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('snippets/add', views.add_snippet_page, name='snippets_add'),
     path('snippets/list', views.snippets_page, name='snippets_list'),
     path('snippet/<int:id>', views.snippet_detail_page, name='snippet_detail'),
-    #path('snippet/create', views.snippet_create, name='snippet_create'),
+    path('snippet/edit/<int:id>', views.snippet_edit_page, name='snippet_edit'),
     path('snippet/delete', views.snippet_delete, name='snippet_delete'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
