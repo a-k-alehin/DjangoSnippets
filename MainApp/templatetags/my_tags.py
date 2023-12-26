34246c85e78142b7b28ac0e1a6c20c92dc08213c
Alexander Alehin <alehin@tcinet.ru>
2023-12-26 15:57:58 +0300

    Шаблонный фильтр

---
+++ MainApp/templatetags/my_tags.py
@@ -0,0 +1,16 @@
+from django import template
+register = template.Library()
+
+
+def is_empty(value, alt):
+   if value:
+       return value
+   return alt
+
+
+register.filter('is_empty', is_empty)
+
+
+@register.filter
+def br(value):
+    return value.replace('\n', '<br>')
